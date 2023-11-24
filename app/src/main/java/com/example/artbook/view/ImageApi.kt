dbb948ae2bf8630b06527ce7e6f408c1a29fe2d7
asmitaxsingh <asmita.singh@innobitsystems.com>
2023-11-24 16:58:14 +0530

    final commit

---
+++ app/src/main/java/com/example/artbook/view/ImageApi.kt
@@ -0,0 +1,12 @@
+package com.example.artbook.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.artbook.R
+
+class ImageApi:Fragment(R.layout.fragement_img_api) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/artbook/view/fragmentArtDetails.kt
@@ -0,0 +1,13 @@
+package com.example.artbook.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.artbook.R
+
+class FragmentArtDetails : Fragment(R.layout.fragment_art_details) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
